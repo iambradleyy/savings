821504a4cce73a957571459b44ac8ec6992b9c36
Bradley Kirk <iambradleyy@gmail.com>
2016-07-13 13:38:19 +0100

    Add dynamic username to navbar.

--- savings.js
+++ savings.js
@@ -1,11 +1,15 @@
 // prefill the form if we have stored data
-$("#input-name").val(localStorage.getItem('username'));
+if (localStorage.getItem("username") !== null) {
+    $("#input-name").val(localStorage.getItem('username'));
+    $("#username").html(localStorage.getItem('username'));
+}
 $("#input-targetamount").val(localStorage.getItem('targetAmount'));
 
 // handle form submission
 $("#form-name").submit(function(e) {
     var username = $("#input-name").val();
     localStorage.setItem('username', username);
+    $("#username").html(username);
     var targetAmount = $("#input-targetamount").val();
     localStorage.setItem('targetAmount', targetAmount);
     e.preventDefault();
